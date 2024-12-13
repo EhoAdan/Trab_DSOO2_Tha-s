aa09e0ee69d80d7c26d2e098e360f9e087cb8b2c
EhoAdan <adansamuelpruss@gmail.com>
2024-12-13 01:02:09 -0300

    Update tela_jogador.py
    Além das mudanças que fiz ante mesmo de dar upload nos arquivos, fiz algum progresso com a interace.

--- tela_jogador.py
+++ tela_jogador.py
@@ -11,12 +11,70 @@ class TelaJogador():
 
     def mostra_lista_jogadores(self, lista_jogadores):
         #sg.theme_previewer()
-        sg.ChangeLookAndFeel('DarkTeal4')
+        sg.ChangeLookAndFeel('NeonYellow1')
         layout = [
             [sg.Text("Lista de Jogadores", font=("Helvica", 25))],
             [sg.PopupScrolled(*lista_jogadores, title="Lista de Jogadores")]
                 ]
         self.__window.close()
+    
+    def jogar_partida(self, partidas_jogadas):
+        sg.ChangeLookAndFeel('NeonYellow1')
+        layout = [
+            [sg.Text("Você jogou uma partida", font=("Helvica", 25))],
+            [sg.PopupScrolled(f"Você já jogou {partidas_jogadas} partidas", title="Você jogou uma partida")]
+                ]
+        self.__window.close()
+
+    def mostra_partidas_jogadas(self, partidas_jogadas):
+        sg.ChangeLookAndFeel('NeonYellow1')
+        layout = [
+            [sg.Text("Partidas Jogadas", font=("Helvica", 25))],
+            [sg.PopupScrolled(f"Você já jogou {partidas_jogadas} partidas")]
+                ]
+        self.__window.close()
+    
+    def lista_amigos(self, amigos):
+        sg.ChangeLookAndFeel('NeonYellow1')
+        layout = [
+            [sg.Text("Esta é sua lista atual de amigos:", font=("Helvica", 25))],
+            [sg.PopupScrolled(*amigos, title="Esta é sua lista atual de amigos:")]
+                ]
+        self.__window.close()
+
+    def lista_amigos_zero(self):
+        sg.ChangeLookAndFeel('NeonYellow1')
+        layout = [
+            [sg.Text("Esta é sua lista atual de amigos:", font=("Helvica", 25))],
+            [sg.PopupScrolled("Você não tem nenhum amigo :(", title="Esta é sua lista atual de amigos:")]
+                ]
+        self.__window.close()
+
+
+    def tela_estats(self, mais_dinheiro_gasto, mais_presentes_dados, \
+                    mais_partidas_jogadas, mais_itens, \
+                    jog_mais_dinheiro_gasto, jog_mais_presenteador, \
+                    jog_mais_partidas, jog_mais_itens):
+    
+        layout = [
+            [sg.Text("Estatísticas dos Jogadores", font=("Helvetica", 16), justification='center')],
+            [sg.Text(f"O jogador que mais investiu no jogo foi: {jog_mais_dinheiro_gasto.nome}")],
+            [sg.Text(f"Com um aporte total de: {mais_dinheiro_gasto}")],
+            [sg.Text(f"O jogador candidato à Papai Noel é: {jog_mais_presenteador.nome}")],
+            [sg.Text(f"Presenteando um total de: {mais_presentes_dados} vezes")],
+            [sg.Text(f"O jogador ProPlayer do momento é: {jog_mais_partidas.nome}")],
+            [sg.Text(f"Jogando {mais_partidas_jogadas} partidas")],
+            [sg.Text(f"O jogador com mais colecionáveis no momento é: {jog_mais_itens.nome}")],
+            [sg.Text(f"Com um total de: {mais_itens} colecionáveis")],
+            [sg.Text("Itens do jogador com mais colecionáveis:")],
+            [sg.Multiline("\n".join([item.nome for item in jog_mais_itens.lista_itens_jogador]), size=(50, 10), disabled=True)],
+            [sg.Button("Fechar")]
+        ]
+
+        # Criação da janela
+        window = sg.Window("Estatísticas dos Jogadores", layout)
+
+        window.close()
 
     def abre_tela(self):
         self.init_opcoes()
@@ -37,7 +95,7 @@ class TelaJogador():
         return opcao
 
     def init_opcoes(self):
-        sg.ChangeLookAndFeel('DarkTeal4')
+        sg.ChangeLookAndFeel('NeonYellow1')
         layout = [
             [sg.Text('O que você quer fazer?', font=("Helvica", 25))],
             [sg.Text('Escolha sua opção', font=("Helvica", 15))],
@@ -71,7 +129,7 @@ class TelaJogador():
         return opcao
 
     def acoes_opcoes_login(self):
-        sg.ChangeLookAndFeel('DarkTeal4')
+        sg.ChangeLookAndFeel('NeonYellow1')
         layout = [
             [sg.Text('O que você quer fazer?', font=("Helvica", 25))],
             [sg.Text('Escolha sua opção', font=("Helvica", 15))],
